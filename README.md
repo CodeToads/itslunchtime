2edd21d31399f58017f7a9020b01a9f3559c2a8e
Jonathan Huang <jhuan015@ucr.edu>
2016-05-30 14:40:33 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Lunch Time #
+
+This App was made to help you and your friends decide what to eat for lunch!
+  > For the indecisive ones
+
+## Summary ##
+  This application will help you decide what to eat for lunch with your friends.  Simply add your choices to the list and it will randomly select one for you to go to.  Once it is added, it will not repeat until next week.  Now you don't have to wait for your coworkers to pick!  More time for lunch.
+
+## Problem ##
+  Fixes indecisiveness so more time for lunching.
+
+## Solution ##
+  Remembers what you ate and what you want.
+
+## Quote from You ##
+  'I never think about what to eat for lunch again!'
+
+## How to Get Started ##
+  *Enter Lunch Choices
+  *Click Lunch Time button
+  *???
+  *Eat Lunch!
+
+## Customer Quote ##
+  'I have more lunch time now thanks to this app.'
+
+## Closing ##
+  Get Grubbing!
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const webpack = require('webpack');
+
+module.exports = {
+
+  devtool: 'eval',
+
+  context: __dirname,
+
+  entry: [
+    './client/app/jsx/boot.jsx'
+  ],
+
+  output: {
+    path: path.join(__dirname, '/client/app'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+   new webpack.NoErrorsPlugin()
+  ],
+
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+
+  module: {
+      loaders: [
+        { test: /\.jsx?$/,
+          loader: 'babel',
+          exclude: path.join(__dirname, 'node_modules'),
+        },
+        { test: /\.scss?$/,
+          loader: 'style!css!sass',
+          include: path.join(__dirname, 'src', 'styles'),
+        },
+        { test: /\.png$/,
+          loader: 'file',
+        },
+        { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+          loader: 'file',
+        },
+        { test: /\.json$/,
+          loader: 'json-loader'
+        }
+      ],
+    },
+    externals: {
+      'react/addons': true,
+      'react/lib/ExecutionEnvironment': true,
+      'react/lib/ReactContext': true
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx', '.json']
+    }
+};

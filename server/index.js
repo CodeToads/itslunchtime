79b95002306d244b9348c575376391b4929e1eee
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-31 21:30:49 -0400

    (feat) Add basic server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+// ROUTING
+var app = require('./server');
+var http = require('http').Server(app);
+// ENV variables
+var port = process.env.PORT || 3000;
+
+http.listen(port, function () {
+  console.log('ROUTING server listening on port: ' + port);
+});

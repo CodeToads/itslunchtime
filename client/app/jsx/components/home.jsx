2e801ac47247c5d9d86afd9772d837eb425b539f
Jonathan S. Huang <jhuan015@ucr.edu>
2016-06-15 15:49:01 -0700

    (Style) Add Material UI Styles

--- client/app/jsx/components/home.jsx
+++ client/app/jsx/components/home.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { test } from '../actions/actions';
+import mui from 'material-ui';
+import AppBar from 'material-ui/AppBar';
+import Card from 'material-ui/Card';
+import List from 'material-ui/List';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Welcome to It's Lunch Time.
+        <AppBar title="It's Lunch Time" />
+        <div className="main">
+          <Card className="c1">
+            <List>
+              Welcome to It's Lunch Time.
+            </List>
+          </Card>
+          <Card className="c2">
+            <List>
+              test
+            </List>
+          </Card>
+        </div>
         <p>{this.props.phrase}</p>
         <button onClick={this.props.test}>press me</button>
       </div>
--- client/app/jsx/main.jsx
+++ client/app/jsx/main.jsx
@@ -6,6 +6,29 @@ import { Router, browserHistory } from 'react-router';
 import reducers from './reducers/rootReducer';
 import routes from './routes';
 import promise from 'redux-promise';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+import {red400, red300, red100, cyanA200} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: red400,
+    primary2Color: red300,
+    primary2Color: red100,
+    accent1Color: cyanA200,
+  },
+  appBar: {
+    height: 60,
+  },
+});
 
 const createStoreWithMiddleware = applyMiddleware(
   promise
@@ -13,7 +36,9 @@ const createStoreWithMiddleware = applyMiddleware(
 
 const App = () => (
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} routes={routes} />
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <Router history={browserHistory} routes={routes} />
+    </MuiThemeProvider>
   </Provider>
 )
 

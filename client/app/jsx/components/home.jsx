95b6d3478d95a693bb5e1bf856929c9fd8d4040e
Jonathan S. Huang <jhuan015@ucr.edu>
2016-06-16 11:58:00 -0700

    (feat) Add Login Button and fix component layout

--- client/app/jsx/components/home.jsx
+++ client/app/jsx/components/home.jsx
@@ -5,6 +5,12 @@ import mui from 'material-ui';
 import AppBar from 'material-ui/AppBar';
 import {Card, CardHeader} from 'material-ui/Card';
 import List from 'material-ui/List';
+import IconButton from 'material-ui/IconButton';
+import MenuItem from 'material-ui/MenuItem';
+import IconMenu from 'material-ui/IconMenu';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import RaisedButton from 'material-ui/RaisedButton';
+
 const RandomAdds = require('./randomAdds');
 const SavedLists = require('./savedLists');
 
@@ -12,15 +18,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        <AppBar title="It's Lunch Time" />
-        <div className="main">
+        <AppBar title="It's Lunch Time" 
+          children={<div><RaisedButton className="login" label="Login/Sign up" secondary={true} /></div>}
+          showMenuIconButton={false}
+          iconElementLeft={<IconButton></IconButton>}
+        />
+        <div className="left">
           <RandomAdds />
+          <SavedLists />
+        </div>
+        <div className="right">
           <Card className="c2">
             <List>
               test
             </List>
           </Card>
-          <SavedLists />
         </div>
         <p>{this.props.phrase}</p>
         <button onClick={this.props.test}>press me</button>
